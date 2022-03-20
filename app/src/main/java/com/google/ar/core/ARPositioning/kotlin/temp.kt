cc50a4ca84b64309f30b03d00f86f700c0b337d0
Akex <1115220867@qq.com>
2022-03-21 02:11:44 +0800

    更改包名、应用名、主页面名
    - 更改包的名字为com.google.ar.core.ARPositioning.kotlin.Transfer
    - 更改应用id为com.Alex.ar.core.kotlin.ARPositioning
    - 更改主页面的类名为mainActivity
    - 更改显示类名为ArView
    - 更改渲染类名为ArRenderer

---
+++ app/src/main/java/com/google/ar/core/ARPositioning/kotlin/temp.kt
@@ -0,0 +1,11 @@
+package com.google.ar.core.ARPositioning.kotlin
+
+
+//private fun updateRecordButton() {
+//    val buttonView: View = findViewById(R.id.record_button)
+//    val button = buttonView as Button
+//    when (appState) {
+//        Temp.AppState.Idle -> button.text = "Record"
+//        Temp.AppState.Recording -> button.text = "Stop"
+//    }
+//}
